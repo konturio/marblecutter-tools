e653f81c225a4848a1c242f203ba9f1f2eba9a1e
Aliaksandr Kalenik <kalenik.aliaksandr@gmail.com>
2023-03-17 10:20:46 +0300

    remove suppress of errors coming from jq

--- bin/process.sh
+++ bin/process.sh
@@ -131,15 +131,15 @@ function update_aws_credentials() {
 
       if [[ ! -z "$profile" ]]; then
         local credentials=$(curl -sf --connect-timeout 1 "http://169.254.169.254/latest/meta-data/iam/security-credentials/${profile}")
-        export AWS_ACCESS_KEY_ID=$(jq -r .AccessKeyId <<< $credentials 2> /dev/null)
-        export AWS_SECRET_ACCESS_KEY=$(jq -r .SecretAccessKey <<< $credentials 2> /dev/null)
-        export AWS_SESSION_TOKEN=$(jq -r .Token <<< $credentials 2> /dev/null)
+        export AWS_ACCESS_KEY_ID=$(jq -r .AccessKeyId <<< $credentials)
+        export AWS_SECRET_ACCESS_KEY=$(jq -r .SecretAccessKey <<< $credentials)
+        export AWS_SESSION_TOKEN=$(jq -r .Token <<< $credentials)
       fi
     else
       local credentials=$(curl -sf --connect-timeout 1 "http://169.254.170.2${AWS_CONTAINER_CREDENTIALS_RELATIVE_URI}")
-      export AWS_ACCESS_KEY_ID=$(jq -r .AccessKeyId <<< $credentials 2> /dev/null)
-      export AWS_SECRET_ACCESS_KEY=$(jq -r .SecretAccessKey <<< $credentials 2> /dev/null)
-      export AWS_SESSION_TOKEN=$(jq -r .Token <<< $credentials 2> /dev/null)
+      export AWS_ACCESS_KEY_ID=$(jq -r .AccessKeyId <<< $credentials)
+      export AWS_SECRET_ACCESS_KEY=$(jq -r .SecretAccessKey <<< $credentials)
+      export AWS_SESSION_TOKEN=$(jq -r .Token <<< $credentials)
     fi
 
     if [[ -z "$AWS_ACCESS_KEY_ID" ]]; then
